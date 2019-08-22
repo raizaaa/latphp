f28e17b332f52137e2ad14f73d0a142cabe902c7
raizaaa <raizapitriani@gmail.com>
2019-08-22 11:07:44 +0700

    Form

---
+++ input08.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <title>Film Kartun Favorit</title>
+</head>
+<body>
+    <form action="proses08.php" method="POST" name="input">
+        <h2>Pilih Film Kartun Favorit Anda :</h2> 
+        <select name="kartun">
+        <option value="Sponge Bob">Sponge Bob</option>
+        <option value="Sinchan">Sinchan</option>
+        <option value="Conan">Conan</option>
+        <option value="Doraemon">Doraemon</option>
+        <option value="Dragon Ball">Dragon Ball</option>
+        <option value="Naruto">Naruto</option>
+        </select>   
+        <input type="submit" name="pilih" value="pilih">
+    </form>
+</body>
+</html>
---
+++ input09.php
@@ -0,0 +1,10 @@
+<html>
+<head><title>Kritik dan Saran</title></head>
+<body>
+    <form action="proses09.php" method="POST" name="input">
+    <h2>Input Kritik / Saran :</h2>
+    <textarea name="saran" cols="40" rows="5"></textarea><br>
+    <input type="submit" name="Proses" value="Input Saran">
+    </form>
+</body>
+</html>
